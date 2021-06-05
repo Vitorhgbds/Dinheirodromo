5e8dbd83bfdb12675ff3b8201578ce24849b9137
vitorhgbds <vitor_hgbds@hotmail.com>
2021-06-05 01:24:57 -0300

    automatizando escolhas de sites arbitrária

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 async function changePage(ms) {
-    setTimeout(window.location.href = 'https://fe.seg.br/',ms);
+    var listaDeSites = ['https://ajuda.locaweb.com.br/wiki/golpes-virtuais/',
+            'https://economia.uol.com.br/noticias/redacao/2021/04/30/veja-quais-sao-os-golpes-virtuais-mais-comuns-e-como-se-proteger.htm']
+    setTimeout(window.location.href = listaDeSites[Math.round(Math.random())],ms);
 }
